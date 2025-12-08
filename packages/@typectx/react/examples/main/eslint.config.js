848f61ff40ae7ec0f8710164b5eca6a2a2a5e5e2
someone635 <someone635@users.noreply.github.com>
2025-12-08 13:18:03 -0500

    update react example link in docs (#53)

--- packages/@typectx/react/examples/main/eslint.config.js
+++ packages/@typectx/react/examples/main/eslint.config.js
@@ -1,8 +1,37 @@
-import baseConfig from "@config.typectx/eslint"
+import js from "@eslint/js"
+import tseslint from "typescript-eslint"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
 import { defineConfig } from "eslint/config"
 import reactHooks from "eslint-plugin-react-hooks"
 
 export default defineConfig([
-    ...baseConfig,
+    // TypeScript specific config
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.strict,
+    {
+        files: ["**/*.{ts,tsx}"],
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+            "@typescript-eslint/no-empty-function": "error"
+        }
+    },
+    // JavaScript specific config
+    {
+        files: ["**/*.{js,mjs,cjs,jsx}"],
+        plugins: {
+            js
+        },
+        extends: ["js/recommended"],
+        rules: {
+            "require-atomic-updates": "error",
+            "no-await-in-loop": "error",
+            "no-constant-condition": "off",
+            "no-redeclare": "off",
+            "no-unused-vars": "off"
+        }
+    },
+    // Prettier config
+    eslintConfigPrettier,
     reactHooks.configs.flat["recommended-latest"]
 ])
