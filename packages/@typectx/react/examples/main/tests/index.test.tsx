771e2e6876b460cd73d4384c48ef0525e90bf4b2
someone635 <felix.dube404@gmail.com>
2025-12-08 05:35:18 -0500

    remove outdated @typectx/react test

--- packages/@typectx/react/examples/main/tests/index.test.tsx
+++ packages/@typectx/react/examples/main/tests/index.test.tsx
@@ -4,69 +4,33 @@ import { render, screen, waitFor } from "@testing-library/react"
 import { QueryClientProvider } from "@tanstack/react-query"
 import { queryClient } from "@/query"
 import { StrictMode } from "react"
-import { createMarket } from "typectx"
-import { use$ } from "../../../@typectx/react/dist"
+import { index } from "typectx"
+import { ctx } from "@/context"
 
 describe("React Client", () => {
     it("should be able to render the app", async () => {
-        const App = $$App.assemble({}).unpack()
+        const App = $$App
+            .assemble(index(ctx.$$defaultUser.pack("userA")))
+            .unpack()
 
         function Test() {
             return <p>Test</p>
         }
 
-        const test = <Test />
         expect(App).toBeDefined()
         render(
             <StrictMode>
                 <QueryClientProvider client={queryClient}>
-                    <App defaultUserId="userA" />
+                    <App />
                 </QueryClientProvider>
             </StrictMode>
         )
-        expect(screen.getByText("Loading default user...")).toBeInTheDocument()
+        expect(screen.getByText("Loading users...")).toBeInTheDocument()
 
         await waitFor(() => {
             expect(
                 screen.getByText("Social Feed Wireframe")
             ).toBeInTheDocument()
         })
-    })
-
-    it("should not rerender stateful client components on reassembly", () => {
-        const market = createMarket()
-        // create a vitest spy
-        const spyA = vi.fn()
-        const spyB = vi.fn()
-        const $$ComponentA = market.offer("ComponentA").asProduct({
-            factory: (init$) => {
-                spyA()
-                return () => {
-                    const $ = use$(init$)
-                    spyB()
-                    return <div>ComponentA</div>
-                }
-            }
-        })
-        const $$ComponentB = market.offer("ComponentB").asProduct({
-            suppliers: [$$ComponentA],
-            factory: (init$, $$) => () => {
-                const $ = use$(init$)
-                $$($$ComponentA).assemble({}).unpack()
-                $$($$ComponentA).assemble({}).unpack()
-                $$($$ComponentA).assemble({}).unpack()
-                const ComponentA = $$($$ComponentA).assemble({}).unpack()
-                return (
-                    <div>
-                        <p>ComponentB</p>
-                        <ComponentA />
-                    </div>
-                )
-            }
-        })
-
-        $$ComponentB.assemble({}).unpack()
-        expect(spyA).toHaveBeenCalledTimes(1)
-        expect(spyB).toHaveBeenCalledTimes(4)
     })
 })
